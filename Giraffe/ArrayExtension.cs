6dd1922c2f39a94a7436585a867b53126dc4e889
franRob <jnp775@gmail.com>
2020-04-25 13:13:50 -0400

    added ArrayExtension as an example of working with arrays

---
+++ Giraffe/ArrayExtension.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Giraffe
+{
+    public static class ArrayExtension
+    {
+        /// <summary>
+        /// simple method that uses Array.IndexOf() to find the element 
+        /// and index# in an array.
+        /// got an example from here: https://www.techiedelight.com/find-index-of-element-array-csharp/
+        /// </summary>
+        /// <returns>element</returns>
+        /// <returns>index</returns>
+        public static int GetIndexOfArray<T>(this T[] array, T item)
+        {
+            //return the index of the element I want to display
+            return Array.IndexOf(array, item);
+        }
+    }
+}
--- Giraffe/Program.cs
+++ Giraffe/Program.cs
@@ -8,17 +8,36 @@ namespace Giraffe
 {
     public class Program
     {
+        public int[] array;
+
         static void Main(string[] args)
         {
+            //example of how to call a static method here: https://www.c-sharpcorner.com/UploadFile/abhikumarvatsa/static-methods-in-C-Sharp/
+
             //Calls SimpleCalculator method to console
-            //SimpleCalculator calculator = new SimpleCalculator();
-            //double sumOfTwoDoubles = calculator.AddTwoDoubles();
-            //Console.WriteLine($"The sum of these 2 values is: {sumOfTwoDoubles}\n");
+            SimpleCalculator calculator = new SimpleCalculator();
+            double sumOfTwoDoubles = calculator.AddTwoDoubles();
+            Console.WriteLine($"The sum of these 2 values is: {sumOfTwoDoubles}\n");
 
             //Calls MadLibs method to console
             MadLibs madLibsPhrase = new MadLibs();
             string showMadLibs = madLibsPhrase.PlayMadLibs();
-            Console.WriteLine($"{showMadLibs}");
+            Console.WriteLine($"{showMadLibs}\n");
+
+            //Calls GetIndexOfArray to console
+            int[] array = { 3, 15, 7, 10, 18, 6 };
+            int item = 7;
+
+            int index = array.GetIndexOfArray(item);
+            if (index != -1)
+            {
+                Console.WriteLine(string.Format("Element {0} found at index {1} \n", item, index));
+            }
+            else
+            {
+                Console.WriteLine("Element not found in array.\n");
+            }
+
         }
     }
 }
