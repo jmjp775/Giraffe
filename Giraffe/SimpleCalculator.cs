3a8da5e1a7d73b13fd98631cd699c50162ab2ebf
franRob <jnp775@gmail.com>
2020-05-10 12:59:06 -0400

    Still wip, need to fix validation of operator and return it to program

--- Giraffe/SimpleCalculator.cs
+++ Giraffe/SimpleCalculator.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,24 +10,64 @@ namespace Giraffe
     public class SimpleCalculator
     {
         /// <summary>
-        /// Method that adds to numbers of type double.
+        /// Method that calculates two numbers of type double, depending on what the operator is.
         /// </summary>
-        /// <returns>SumOfTwoNumbers</returns>
-        public double AddTwoDoubles()
+        /// <returns>result</returns>
+        public double CalculateTwoNumbers()
         {
-            ///Summary
-            ///A simple calculator
+            //string operator;
+            string[] operatorArray = {"+", "-", "*", "/" };
+
             Console.Write("Enter a number: ");
-             double num1 = Convert.ToDouble(Console.ReadLine());
+            double num1 = Convert.ToDouble(Console.ReadLine());
 
             Console.Write("Enter another number: ");
-             double num2 = Convert.ToDouble(Console.ReadLine());
+            double num2 = Convert.ToDouble(Console.ReadLine());
 
-            var SumOfTwoNumbers = (num1 + num2);
-            return SumOfTwoNumbers;
+            Console.Write("Enter an operator: ");
+            string operatorAsString = Console.ReadLine();
 
-            //Console.ReadLine();
+            if (operatorArray.Any(operatorAsString.Contains))
+            {
+                if (operatorAsString == "+")
+                {
+                    var result = (num1 + num2);
+                    return result;
+                }
+                else if (operatorAsString == "-")
+                {
+                    var result = (num1 - num2);
+                    return result;
+                }
+                else if (operatorAsString == "/")
+                {
+                    var result = (num1 / num2);
+                    return result;
+                }
+                else if (operatorAsString == "*")
+                {
+                    var result = (num1 * num2);
+                    return result;
+                }
+            }
+            else
+            {
+                Console.WriteLine("This is an invalid operator. Please enter a valid operator: ");
+                operatorAsString = Console.ReadLine();
+            }
+
+
+
+
+
+
         }
-        
+
+        private bool IsOperatorValid(string operator)
+        {
+            string[] operatorArray = { "+", "-", "*", "/" };
+            
+        }
+
     }
 }
